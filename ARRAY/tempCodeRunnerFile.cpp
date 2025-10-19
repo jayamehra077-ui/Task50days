6ae9714342d3e93f4675f2bac885b850e8c2c54f
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-19 23:12:23 +0530

    CODE

--- ARRAY/tempCodeRunnerFile.cpp
+++ ARRAY/tempCodeRunnerFile.cpp
@@ -2,40 +2,38 @@
 #include<conio.h>
 int main()
 {
-    int arr1[50],arr2[50];
-    int n,m;
-    printf("enter no of elements in first array:\n");
-    scanf("%d",&n);
-    printf("enter elements:\n");
-    for(int i=0;i<n;i++)
-    {
-     scanf("%d",&arr1[i]);
-    }
-     for(int i=0;i<n;i++)
-    {
-     printf(" %d ",arr1[i]);
-    }
-    printf("\n");
-    printf("enter no of elements in second array:\n");
-    scanf("%d",&m);
-    printf("enter elements:\n");
-    for(int i=0;i<m;i++)
-    {
-     scanf("%d",&arr2[i]);
-    }
-     for(int i=0;i<m;i++)
-    {
-     printf(" %d ",arr2[i]);
-    }
-    printf("\n");
-    printf("sum of two arrays:\n");
-    int arr3[50];
-    for(int i=0;i<n;i++)
-    {
-        arr3[i]=arr1[i]-arr2[i];
-    }
-    for(int i=0;i<n;i++)
-    {
-     printf(" %d ",arr3[i]);
-    }
+ int arr[50][50],m,n=0;
+ printf("enter no of rows:\n");
+ scanf("%d",&m);
+ printf("enter no of columns:\n");
+ scanf("%d",&n);
+ printf("enter elements:\n");
+ for(int i=0;i<m;i++)
+ {
+   for(int j=0;j<n;j++)
+   {
+printf("ELEMENT arr[%d][%d]:",i,j);
+scanf("%d",&arr[i][j]);
+   } 
+ }
+ for(int i=0;i<m;i++)
+ {
+   for(int j=0;j<n;j++)
+   {
+//printf("ELEMENT arr[%d][%d]:",i,j);
+printf(" %d ",arr[i][j]);
+   } 
+   printf(" \n ");
+ }
+ printf("Transpose of matrix:\n");
+ for(int i=0;i<m;i++)
+ {
+   for(int j=0;j<n;j++)
+   {
+//printf("ELEMENT arr[%d][%d]:",i,j);
+printf(" %d ",arr[j][i]);
+   } 
+   printf(" \n ");
+ }
+
 }
