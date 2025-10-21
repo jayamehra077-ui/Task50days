e6f31c37a3410a8b26697e2e769542d6967749fe
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-21 21:28:45 +0530

    CODE

--- ARRAY/tempCodeRunnerFile.cpp
+++ ARRAY/tempCodeRunnerFile.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 int main()
 {
- int arr[50][50],m,n=0,sum=0,avg;
+ int arr[50][50],m,n=0;
  printf("enter no of rows:\n");
  scanf("%d",&m);
  printf("enter no of columns:\n");
@@ -25,18 +25,17 @@ printf(" %d ",arr[i][j]);
    } 
    printf(" \n ");
  }
- printf("Average of elements:\n");
+ int max=0;
  for(int i=0;i<m;i++)
  {
    for(int j=0;j<n;j++)
-   {int x=m*n;
-    sum=sum+arr[i][j];
-    avg=sum/x;
-//printf("ELEMENT arr[%d][%d]:",i,j);
-//scanf("%d",&arr[i][j]);
-   
+   {
+    if(arr[i][j]>max)
+    {
+        max=arr[i][j];
+    }
    } 
  }
- //printf("%d",sum);
-printf("%d",avg);
+ printf("Maximum Element=%d",max);
+
 }
