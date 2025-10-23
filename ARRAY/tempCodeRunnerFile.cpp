2a101c9059f357937907f33d3dc65b58a5f4a275
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-23 22:19:05 +0530

    CODE

--- ARRAY/tempCodeRunnerFile.cpp
+++ ARRAY/tempCodeRunnerFile.cpp
@@ -1,42 +1,32 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
- int arr[50][50],m,n=0;
- printf("enter no of rows:\n");
- scanf("%d",&m);
- printf("enter no of columns:\n");
- scanf("%d",&n);
- printf("enter elements:\n");
- for(int i=0;i<m;i++)
- {
-   for(int j=0;j<n;j++)
-   {
-printf("ELEMENT arr[%d][%d]:",i,j);
-scanf("%d",&arr[i][j]);
-   } 
- 
- }
- for(int i=0;i<m;i++)
- {
-   for(int j=0;j<n;j++)
-   {
-//printf("ELEMENT arr[%d][%d]:",i,j);
-printf(" %d ",arr[i][j]);
-   } 
-   printf(" \n ");
- }
- printf("Even Numbers Are:\n");
-for(int i=0;i<m;i++)
- {
-   for(int j=0;j<n;j++)
-   {
-    if(arr[i][j]%2!=0)
+    int a[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    for(int i=0;i<3;i++)
     {
-        printf(" %d ",arr[i][j]);
+        for(int j=0;j<3;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
     }
-   } 
- 
- }
+
+    int a1[9];
+    int k=0;
+    printf("converted into 1D aaray:\n");
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            a1[k]=a[i][j];
+            k++;
+        }
+    }
+    
+    for(int k=0;k<9;k++)
+    {
+   // printf("converted into 1D array:\n");
+    printf("%d ",a1[k]);
+    }
 
 }
