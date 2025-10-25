d91234e2ee5d6123b1b7ae9734a3989d3cfec71e
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-25 21:50:22 +0530

    CODE

--- ARRAY/tempCodeRunnerFile.cpp
+++ ARRAY/tempCodeRunnerFile.cpp
@@ -2,35 +2,45 @@
 #include<conio.h>
 int main()
 {
-    int arr[50];
-    int n;
-    
-    printf("enter no of elements in array:\n");
-    scanf("%d",&n);
-    printf("enter elements:\n");
-    for(int i=0;i<n;i++)
-    {
-     scanf("%d",&arr[i]);
-    }
-     for(int i=0;i<n;i++)
-    {
-     printf(" %d ",arr[i]);
-    }
-    printf("\n");
-    int x;
+ int arr[50][50],m,n=0;
+ printf("enter no of rows:\n");
+ scanf("%d",&m);
+ printf("enter no of columns:\n");
+ scanf("%d",&n);
+ printf("enter elements:\n");
+ for(int i=0;i<m;i++)
+ {
+   for(int j=0;j<n;j++)
+   {
+printf("ELEMENT arr[%d][%d]:",i,j);
+scanf("%d",&arr[i][j]);
+   } 
+ }
+ for(int i=0;i<m;i++)
+ {
+   for(int j=0;j<n;j++)
+   {
+//printf("ELEMENT arr[%d][%d]:",i,j);
+printf(" %d ",arr[i][j]);
+   } 
+   printf(" \n ");
+ }
+int x;
     //int found_index = -1;
     printf("enter no. to be search:\n");
     scanf("%d",&x);
     int found_index = -1;
     for(int i=0;i<n;i++)
     {
-     if(arr[i]==x)
+        for(int j=0;j<n;j++){
+     if(arr[i][j]==x)
      {
         found_index = i; // Element found, store its index
             break;           
         //printf("Element %d found at index %d.\n", x, i);
      }
     }
+}
     if (found_index != -1) {
         printf("Element %d found at index %d.\n", x, found_index);
     }
@@ -41,4 +51,5 @@ int main()
     
     return 0;
     
+
 }
